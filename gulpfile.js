772d71e0b061ddb0e5aa56e4eb2b93ab038dea0f
Tomasz <tomasz.szawara@gmail.com>
2015-06-29 17:02:11 +0100

    added browser-sync watcher

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
 gulp.task('default', function() {
 
@@ -12,4 +14,15 @@ gulp.task('less', function () {
         paths: []
       }))
       .pipe(gulp.dest('app/css'));
+});
+
+// watch files for changes and reload
+gulp.task('serve', function() {
+  browserSync({
+    server: {
+      baseDir: 'app'
+    }
+  });
+
+  gulp.watch(['*.html',  '*.js', 'views/*/*', 'css/*.css'], {cwd: 'app'}, reload);   // cwd = current working dir.
 });
